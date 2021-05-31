d14d937a15030ae185d1ef28b98224a4e90467bd
Max Base <MaxBaseCode@gmail.com>
2021-06-01 02:14:28 +0430

    Add comments to hello.asm

--- hello.asm
+++ hello.asm
@@ -8,13 +8,13 @@ section .text
 	global _start
 
 _start:
-	mov rax, 1     ; rax is 64bit reg
-	mov edi, 1     ; edi is 32bit reg
-	mov rsi, text  ; rsi is 64bit reg
-	mov rdx, 14    ; rdx is 64bit reg
+	mov rax, 1     ; rax is 64bit reg ; first  arg of syscall
+	mov rdi, 1     ; edi is 32bit reg ; second arg of syscall
+	mov rsi, text  ; rsi is 64bit reg ; third  arg of syscall
+	mov rdx, 14    ; rdx is 64bit reg ; fourd  arg of syscall
 	syscall
 	
-	mov rax, 60    ; rax is 64bit reg
-	mov rdi, 0     ; rdi is 64bit reg
+	mov rax, 60    ; rax is 64bit reg ; first  arg of syscall
+	mov rdi, 0     ; rdi is 64bit reg ; second arg of syscall
 	syscall
 
