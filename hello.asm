4dd530529c0e72be704abdcb5c803273b13daced
Max Base <MaxBaseCode@gmail.com>
2021-06-01 02:12:33 +0430

    add comment of registers at hello.asm

--- hello.asm
+++ hello.asm
@@ -2,19 +2,19 @@
 ; https://github.com/BaseMax/FirstAssemblyNASM
 
 section .data
-	text db "Hello, World!", 10
+	text db "Hello, World!", 10 ; 10 equal to \n
 
 section .text
 	global _start
 
 _start:
-	mov rax, 1
-	mov edi, 1
-	mov rsi, text
-	mov rdx, 14
+	mov rax, 1     ; rax is 64bit reg
+	mov edi, 1     ; edi is 32bit reg
+	mov rsi, text  ; rsi is 64bit reg
+	mov rdx, 14    ; rdx is 64bit reg
 	syscall
 	
-	mov rax, 60
-	mov rdi, 0
+	mov rax, 60    ; rax is 64bit reg
+	mov rdi, 0     ; rdi is 64bit reg
 	syscall
 
